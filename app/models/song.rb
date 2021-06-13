170f447b6b54fbcb59db235e5409c495032d2554
Omar Branez <omar.branez@gmail.com>
2021-06-13 18:56:21 -0400

    commit tree 392d9177f6aff4a426b2db5dd5bf91a7d9add3bb

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,44 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name) # create the setter method
+    self.artist = Artist.find_or_create_by(name: name)
+    # sets the artist to the artist of the specified name
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+      # returns that name
+    end
+  end
+
+  def genre_name=(genre) # for genre too
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    if self.genre
+      self.genre.name
+    end
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content != "" #cant allow blanks
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+    # this is the same as self.notes.map { |n| n.content }
+  end
+
+
+
 end
